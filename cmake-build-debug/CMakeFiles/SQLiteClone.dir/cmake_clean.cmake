aeff2783aa0dbce209a275ce6f29016b4e936787
Hjelmar, Brad <Brad.Hjelmar1@T-Mobile.com>
2018-05-31 10:54:01 -0400

    project init

---
+++ cmake-build-debug/CMakeFiles/SQLiteClone.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/SQLiteClone.dir/main.c.o"
+  "SQLiteClone.pdb"
+  "SQLiteClone"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/SQLiteClone.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
